b61b823e3872c911ed3572bcb87bfb8a86d38f77
remy <rahimjangi1982@gmail.com>
2024-07-03 01:29:37 +0200

    Initial commit

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,24 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+var app = builder.Build();
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
