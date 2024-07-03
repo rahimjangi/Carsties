9dccff2fb462eac6d7796f63c9053c668f70b48c
remy <rahimjangi1982@gmail.com>
2024-07-04 00:55:55 +0200

    Seed Data Done! ScopeProvider

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,5 @@
 using AuctionService.Data;
+using AuctionService.RequestHellpers;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,7 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
 var app = builder.Build();
 
@@ -20,5 +22,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+try
+{
+    DbInitializer.InitDB(app);
+}
+catch (Exception e)
+{
+
+    Console.WriteLine(e);
+}
 
 app.Run();
---
+++ src/AuctionService/RequestHellpers/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHellpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<AuctionDto, Auction>();
+        CreateMap<CreateAuctionDto, Auction>()
+        .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+        CreateMap<Item, AuctionDto>();
+
+    }
+}
